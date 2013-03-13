e56ae659975912440b6efbb807993ead0396c140
Agnieszka Inglot <againglot@gmail.com>
2013-03-13 23:24:52 +0100

    13.03.2013

--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -2,5 +2,31 @@ class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
+    @items = items
   end
+
+  def size
+    @items.size
+  end
+
+  def empty?
+    @items.empty?
+  end
+
+  def first
+    return @items[0]
+  end
+
+  def last
+    return @items[-1]
+  end
+
+  def <<(item_description)
+    @items << item_description
+  end
+
+#  def to_s(item_description)
+#    "(#@item_description)"
+#  end
+
 end
